4843597128591140238a1cfbcaccd2f3ff676621
YanZhi <YanZhi@YanZhi-MacBook-Pro.local>
2017-08-20 13:48:09 +0800

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
+    <div>
     <h3>Examples Component!</h3>
+    <p>Welcome to examples pages!</p>
+    </div>
   );
 };
 
